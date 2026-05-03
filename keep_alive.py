426f3ab18c1dda9439747ca1d21504568e10c0eb
lucachak <lucas.lucachak9@gmail.com>
2026-05-03 20:07:23 +0100

    done

--- keep_alive.py
+++ keep_alive.py
@@ -1,25 +1,37 @@
-import urllib.request
-import time
 import datetime
 import random
-
+import time
+import urllib.request
 
 URL = "https://cambiomatic.onrender.com/login"
-# "https://www.cambiomatic.onrender.com"
+URL2 = "https://fingen-zcke.onrender.com/"
+
 
 def ping():
     while True:
         try:
-            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Fazendo request para {URL}...")
+            print(
+                f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Fazendo request para {URL} and {URL2}..."
+            )
             response = urllib.request.urlopen(URL)
-            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Status: {response.getcode()} OK")
+            response2 = urllib.request.urlopen(URL2)
+
+            print(
+                f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Status: {response.getcode()} OK"
+            )
+            print(
+                f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Status: {response2.getcode()} OK"
+            )
         except Exception as e:
-            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Erro: {e}")
-        
+            print(
+                f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Erro: {e}"
+            )
+
         # Espera um tempo aleatório entre 60 (1 minuto) e 90 (1.5 minutos) segundos
         wait_time = random.randint(60, 90)
         print(f"Aguardando {wait_time} segundos para o próximo request...\n")
         time.sleep(wait_time)
+
 
 if __name__ == "__main__":
     print("Iniciando o script de keep-alive...")
